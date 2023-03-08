06c6fc735d0f77cd3b9f10e53d58dee143306eb6
Sander Bos <sanderbos17@icloud.com>
2023-03-08 15:32:45 +0100

    Updated docker compose app

--- 3. Docker Compose/app/app.js
+++ 3. Docker Compose/app/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const { Dog } = require("./models");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/dogs", async (req, res) => {
   const allDogs = await Dog.find();
